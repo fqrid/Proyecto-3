b3ef2d7ccc2d481326e53d180f7d5cd9be67c983
Tati-126 <diazt0346@gmail.com>
2026-02-27 19:53:55 -0500

    Proyecto inicial: backend con Express, MongoDB y nodemon

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+app.listen(5000, () => {
+  console.log("Servidor corriendo en puerto 5000");
+});
