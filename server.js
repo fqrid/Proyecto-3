4a328718c31eca2c7587818338398e38e18dad3a
Niye-create <niyeriethr@gmail.com>
2026-03-06 21:39:55 -0500

    feat: estructura inicial del backend y rutas de preguntas

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import preguntaRoutes from "./routes/preguntaRoutes.js";
+import opcionRespuestaRoutes from "./routes/opcionRespuestaRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -11,9 +13,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Rutas
 app.get("/", (req, res) => {
-  res.send("API funcionando");
+  res.send("API del Proyecto funcionando ✅");
 });
+
+app.use("/api/preguntas", preguntaRoutes);
+app.use("/api/opciones", opcionRespuestaRoutes);
 
 app.listen(5000, () => {
   console.log("Servidor corriendo en puerto 5000");
